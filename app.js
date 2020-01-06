3762361ee3c883ced2954a6b3cc468a0125c4e20
Audrey Odoemenam <audreypx100@gmail.com>
2020-01-06 13:12:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var inputLabel = document.getElementById('inputLabel');
 function pushBtn(obj) {
      
     var pushed = obj.innerHTML;
-     
+    
     if (pushed == '=') {
         // Calculate
         inputLabel.innerHTML = eval(inputLabel.innerHTML);
          
     } else if (pushed == 'AC') {
-        // All Clear
+        // All Clear button
         inputLabel.innerHTML = '0';
          
     } else {
